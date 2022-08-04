1a488808c0e5a008006768f4023a80b7ad788510
kaedemalu <kaedemalu@gmail.com>
2022-08-05 01:19:03 +0900

    initial commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+require'plugins'
+vim.cmd[[autocmd BufWritePost plugins.lua PackerCompile]]
+
+-- For sidebar-nvim
+local sidebar = require("sidebar-nvim")
+local opts = {open = true}
+sidebar.setup(opts)
+
+vim.cmd('colorscheme terafox')
---
+++ lua/plugins.lua
@@ -0,0 +1,31 @@
+vim.cmd[[packadd packer.nvim]]
+
+require'packer'.startup(function()
+  use 'EdenEast/nightfox.nvim'
+
+  use {'neoclide/coc.nvim'}
+  use { 'hashicorp/terraform-ls' }
+  use { 'sidebar-nvim/sidebar.nvim' }
+
+  use{'wbthomason/packer.nvim', opt = true}
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+end)
+
+require('sidebar-nvim').setup({
+  sections = { 'git', 'symbols', 'files' },
+  files = {
+    icon = '🗂',
+    show_hidden = false,
+    ignored_path = {'%.git$'}
+  },
+  symbols = {
+    icon = 'ƒ',
+  },
+  git = {
+    icon = '',
+  }
+})
