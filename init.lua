115366888d729c9f18a16fb256be81d278d64e82
kaedemalu <kaedemalu@gmail.com>
2022-09-02 02:22:00 +0900

    Add shortcut for Telescope.nvim

--- init.lua
+++ init.lua
@@ -15,7 +15,18 @@ require 'lualine'.setup {
 }
 
 -- From .vimrc
-local opt = vim.o
+-- see :help vim.opt
+local opt = vim.opt
 opt.number = true
+opt.visualbell = true
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
 opt.cuc = true -- Cursor Column
 opt.cul = true -- Cursor Line
+
+-- Setting for Telescope.nvim
+-- vim.keymap.set('n', 'sf', '<cmd>lua require("telescope.builtin").find_files(require("telescope.themes").get_ivy({}))<cr>',{noremap = true})
+vim.keymap.set('n', 'sf', '<cmd>lua require("telescope.builtin").find_files()<cr>',{noremap = true})
+vim.keymap.set('n', 'sb', '<cmd>lua require("telescope.builtin").buffers()<cr>',{noremap = true})
+vim.keymap.set('n', 'sh', '<cmd>lua require("telescope.builtin").help_tags()<cr>',{noremap = true})
